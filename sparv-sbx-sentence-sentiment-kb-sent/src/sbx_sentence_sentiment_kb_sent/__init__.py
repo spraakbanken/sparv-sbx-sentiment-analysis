e93df9ed22061f4bc6f6d7324a26638a1d13548d
Kristoffer Andersson <kristoffer.andersson@gu.se>
2024-05-28 13:47:45 +0200

    feat: add num_decimals config

--- sparv-sbx-sentence-sentiment-kb-sent/src/sbx_sentence_sentiment_kb_sent/__init__.py
+++ sparv-sbx-sentence-sentiment-kb-sent/src/sbx_sentence_sentiment_kb_sent/__init__.py
@@ -1,8 +1,18 @@
 """Sparv plugin for annotating sentences with sentiment analysis."""
 
+from sparv import api as sparv_api  # type: ignore [import-untyped]
+
 from sbx_sentence_sentiment_kb_sent.annotations import annotate_sentence_sentiment
 
 __all__ = ["annotate_sentence_sentiment"]
 
 __description__ = "Annotate sentence with sentiment analysis."
 __version__ = "0.1.0"
+
+__config__ = [
+    sparv_api.Config(
+        "sbx_sentence_sentiment_kb_bert.num_decimals",
+        description="The number of decimals to round the score to",
+        default=3,
+    ),
+]
